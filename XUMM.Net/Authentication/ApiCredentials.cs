c07e2321c00c1575e2349d0b881970759d1dfc08
Dominique Blomsma <dominique_blomsma@hotmail.com>
2021-09-13 21:55:15 +0200

    Added the XummClient with ApiCredentials.

---
+++ XUMM.Net/Authentication/ApiCredentials.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace XUMM.Net.Authentication
+{
+    public class ApiCredentials
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+
+        public ApiCredentials(string apiKey, string apiSecret)
+        {
+            if (string.IsNullOrWhiteSpace(apiKey))
+            {
+                throw new ArgumentException($"{nameof(apiKey)} cannot be null or whitespace", nameof(apiKey));
+            }
+
+            if (string.IsNullOrWhiteSpace(apiSecret))
+            {
+                throw new ArgumentException($"{nameof(apiSecret)} cannot be null or whitespace", nameof(apiSecret));
+            }
+
+            ApiKey = apiKey;
+            ApiSecret = apiSecret;
+        }
+    }
+}
---
+++ XUMM.Net/XummClient.cs
@@ -0,0 +1,14 @@
+﻿using XUMM.Net.Authentication;
+
+namespace XUMM.Net
+{
+    public class XummClient
+    {
+        public ApiCredentials Credentials { get; private set; }
+
+        public XummClient(string apiKey, string apiSecret)
+        {
+            Credentials = new ApiCredentials(apiKey, apiSecret);
+        }
+    }
+}
