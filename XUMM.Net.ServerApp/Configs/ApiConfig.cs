6d97aa1f1fe295f27d5c90aa83fd86db49c26517
Dominique Blomsma <d.blomsma@stablefx.net>
2022-01-26 11:38:58 +0100

    Added a Blazor server app with the flow to create a signin payload and subscribe to it.

---
+++ XUMM.Net.ServerApp/Configs/ApiConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace XUMM.Net.ServerApp.Configs;
+
+public class ApiConfig
+{
+    public string Key { get; set; } = default!;
+    public string Secret { get; set; } = default!;
+}
---
+++ XUMM.Net.ServerApp/Controllers/ConfigurationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XUMM.Net.ServerApp.Configs;
+
+namespace XUMM.Net.ServerApp.Controllers;
+
+[ApiController]
+[Route("configuration")]
+public class ConfigurationController : Controller
+{
+    private readonly IConfiguration _configuration;
+
+    public ConfigurationController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("api")]
+    public ApiConfig GetApiConfig()
+    {
+        return _configuration.GetSection("Api").Get<ApiConfig>();
+    }
+}
