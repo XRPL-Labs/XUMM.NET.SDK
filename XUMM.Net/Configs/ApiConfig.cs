64cd058745081f78b887ebc8f2aecc6823b88568
Dominique Blomsma <d.blomsma@stablefx.net>
2022-02-02 17:15:20 +0100

    Added GUID validation to the ApiConfig

--- XUMM.Net/Configs/ApiConfig.cs
+++ XUMM.Net/Configs/ApiConfig.cs
@@ -1,11 +1,43 @@
-﻿namespace XUMM.Net.Configs;
+﻿using System;
+using XUMM.Net.Extensions;
+
+namespace XUMM.Net.Configs;
 
 public class ApiConfig
 {
     internal static readonly string DefaultRestClientAddress = "https://xumm.app/api/v1";
     internal static readonly string DefaultSectionKey = "Xumm";
 
+    private string _apiKey = default!;
+    private string _apiSecret = default!;
+
     public string RestClientAddress { get; set; } = DefaultRestClientAddress;
-    public string ApiKey { get; set; } = default!;
-    public string ApiSecret { get; set; } = default!;
+
+    public string ApiKey
+    {
+        get => _apiKey;
+        set
+        {
+            if (!value.IsValidUuid())
+            {
+                throw new Exception("Invalid API Key.");
+            }
+
+            _apiKey = value;
+        }
+    }
+
+    public string ApiSecret
+    {
+        get => _apiSecret;
+        set
+        {
+            if (!value.IsValidUuid())
+            {
+                throw new Exception("Invalid API Secret.");
+            }
+
+            _apiSecret = value;
+        }
+    }
 }
