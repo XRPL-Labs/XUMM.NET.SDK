a189374bffe406de2ef41a356bee0c965698a8e0
Dominique Blomsma <d.blomsma@stablefx.net>
2022-02-21 21:03:19 +0100

    Added JsonDocument extensions to get the string representation.

---
+++ XUMM.Net/Extensions/JsonDocumentExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace XUMM.Net.Extensions;
+
+public static class JsonDocumentExtensions
+{
+    public static string ToJsonString(this JsonDocument jsonDocument)
+    {
+        using var stream = new MemoryStream();
+        var writer = new Utf8JsonWriter(stream, new JsonWriterOptions
+        {
+            Indented = true
+        });
+        jsonDocument.WriteTo(writer);
+        writer.Flush();
+        return Encoding.UTF8.GetString(stream.ToArray());
+    }
+}
