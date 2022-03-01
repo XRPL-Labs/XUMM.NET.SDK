01f831bf9d504909c5129421f01cf26b68a8fcfa
Dominique Blomsma <d.blomsma@stablefx.net>
2022-03-01 17:12:48 +0100

    Moved JsonDocumentExtensions to the Blazor app

---
+++ XUMM.Net.ServerApp/Extensions/JsonDocumentExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace XUMM.Net.ServerApp.Extensions
+{
+    public static class JsonDocumentExtensions
+    {
+        public static string? ToJsonString(this JsonDocument? jsonDocument)
+        {
+            if (jsonDocument == null)
+            {
+                return null;
+            }
+
+            using var stream = new MemoryStream();
+            var writer = new Utf8JsonWriter(stream, new JsonWriterOptions
+            {
+                Indented = true
+            });
+            jsonDocument.WriteTo(writer);
+            writer.Flush();
+            return Encoding.UTF8.GetString(stream.ToArray());
+        }
+    }
+}
