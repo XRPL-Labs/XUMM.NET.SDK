e242a29eecf05bcc32c611b991d63b839047925e
Dominique Blomsma <d.blomsma@stablefx.net>
2022-03-10 16:44:10 +0100

    Added .NET Core 3.1 WebApp example project

---
+++ examples/XUMM.NET.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace XUMM.NET.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ examples/XUMM.NET.WebApp/Pages/PingPong.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using XUMM.NET.SDK.Clients.Interfaces;
+using XUMM.NET.SDK.Models.Misc;
+
+namespace XUMM.NET.WebApp.Pages
+{
+    public class PingPongModel : PageModel
+    {
+        public XummPong Pong { get; set; }
+
+        private readonly IXummMiscClient _miscClient;
+
+        public PingPongModel(IXummMiscClient miscClient)
+        {
+            _miscClient = miscClient;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Pong = await _miscClient.GetPingAsync();
+        }
+    }
+}
