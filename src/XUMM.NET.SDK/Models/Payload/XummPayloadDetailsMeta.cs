60a4e8ffb539c80a2072f5d81a85848feeccfeeb
Dominique Blomsma <d.blomsma@stablefx.net>
2022-04-19 09:08:16 +0200

    Added optional Signers to XummPayloadDetailsMeta and XummPayloadOptions.

--- src/XUMM.NET.SDK/Models/Payload/XummPayloadDetailsMeta.cs
+++ src/XUMM.NET.SDK/Models/Payload/XummPayloadDetailsMeta.cs
@@ -1,58 +1,60 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace XUMM.NET.SDK.Models.Payload
+namespace XUMM.NET.SDK.Models.Payload;
+
+public class XummPayloadDetailsMeta
 {
-    public class XummPayloadDetailsMeta
-    {
-        [JsonPropertyName("exists")]
-        public bool Exists { get; set; }
+    [JsonPropertyName("exists")]
+    public bool Exists { get; set; }
 
-        [JsonPropertyName("uuid")]
-        public string Uuid { get; set; } = default!;
+    [JsonPropertyName("uuid")]
+    public string Uuid { get; set; } = default!;
 
-        [JsonPropertyName("multisign")]
-        public bool Multisign { get; set; }
+    [JsonPropertyName("multisign")]
+    public bool Multisign { get; set; }
 
-        [JsonPropertyName("submit")]
-        public bool Submit { get; set; }
+    [JsonPropertyName("submit")]
+    public bool Submit { get; set; }
 
-        [JsonPropertyName("destination")]
-        public string Destination { get; set; } = default!;
+    [JsonPropertyName("destination")]
+    public string Destination { get; set; } = default!;
 
-        [JsonPropertyName("resolved_destination")]
-        public string ResolvedDestination { get; set; } = default!;
+    [JsonPropertyName("resolved_destination")]
+    public string ResolvedDestination { get; set; } = default!;
 
-        [JsonPropertyName("resolved")]
-        public bool Resolved { get; set; }
+    [JsonPropertyName("resolved")]
+    public bool Resolved { get; set; }
 
-        [JsonPropertyName("signed")]
-        public bool Signed { get; set; }
+    [JsonPropertyName("signed")]
+    public bool Signed { get; set; }
 
-        [JsonPropertyName("cancelled")]
-        public bool Cancelled { get; set; }
+    [JsonPropertyName("cancelled")]
+    public bool Cancelled { get; set; }
 
-        [JsonPropertyName("expired")]
-        public bool Expired { get; set; }
+    [JsonPropertyName("expired")]
+    public bool Expired { get; set; }
 
-        [JsonPropertyName("pushed")]
-        public bool Pushed { get; set; }
+    [JsonPropertyName("pushed")]
+    public bool Pushed { get; set; }
 
-        [JsonPropertyName("app_opened")]
-        public bool AppOpened { get; set; }
+    [JsonPropertyName("app_opened")]
+    public bool AppOpened { get; set; }
 
-        [JsonPropertyName("opened_by_deeplink")]
-        public bool? OpenedByDeeplink { get; set; }
+    [JsonPropertyName("opened_by_deeplink")]
+    public bool? OpenedByDeeplink { get; set; }
 
-        [JsonPropertyName("openedimmutable_by_deeplink")]
-        public bool? Immutable { get; set; }
+    [JsonPropertyName("openedimmutable_by_deeplink")]
+    public bool? Immutable { get; set; }
 
-        [JsonPropertyName("return_url_app")]
-        public string? ReturnUrlApp { get; set; }
+    [JsonPropertyName("return_url_app")]
+    public string? ReturnUrlApp { get; set; }
 
-        [JsonPropertyName("return_url_web")]
-        public string? ReturnUrlWeb { get; set; }
+    [JsonPropertyName("return_url_web")]
+    public string? ReturnUrlWeb { get; set; }
 
-        [JsonPropertyName("is_xapp")]
-        public bool IsXapp { get; set; }
-    }
+    [JsonPropertyName("is_xapp")]
+    public bool IsXapp { get; set; }
+
+    [JsonPropertyName("signers")]
+    public string[]? Signers { get; set; }
 }
--- src/XUMM.NET.SDK/Models/Payload/XummPayloadOptions.cs
+++ src/XUMM.NET.SDK/Models/Payload/XummPayloadOptions.cs
@@ -1,31 +1,36 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace XUMM.NET.SDK.Models.Payload
+namespace XUMM.NET.SDK.Models.Payload;
+
+public class XummPayloadOptions
 {
-    public class XummPayloadOptions
-    {
-        /// <summary>
-        /// Should the xumm app submit to the XRPL after signing? (Optional)
-        /// </summary>
-        [JsonPropertyName("submit")]
-        public bool? Submit { get; set; }
+    /// <summary>
+    /// Should the xumm app submit to the XRPL after signing? (Optional)
+    /// </summary>
+    [JsonPropertyName("submit")]
+    public bool? Submit { get; set; }
 
-        /// <summary>
-        /// Should the transaction be signed as a multi sign transaction? (Optional)
-        /// </summary>
-        [JsonPropertyName("multisign")]
-        public bool? MultiSign { get; set; }
+    /// <summary>
+    /// Should the transaction be signed as a multi sign transaction? (Optional)
+    /// </summary>
+    [JsonPropertyName("multisign")]
+    public bool? MultiSign { get; set; }
 
-        /// <summary>
-        /// After how many minutes should the payload expire? (Optional)
-        /// </summary>
-        [JsonPropertyName("expire")]
-        public int Expire { get; set; }
+    /// <summary>
+    /// After how many minutes should the payload expire? (Optional)
+    /// </summary>
+    [JsonPropertyName("expire")]
+    public int Expire { get; set; }
 
-        /// <summary>
-        /// Where should the user be redirected to after resolving the payload? (Optional)
-        /// </summary>
-        [JsonPropertyName("return_url")]
-        public XummPayloadReturnUrl? ReturnUrl { get; set; }
-    }
+    /// <summary>
+    /// Force any of the provided accounts to sign. (Optional)
+    /// </summary>
+    [JsonPropertyName("signers")]
+    public string[]? Signers { get; set; }
+
+    /// <summary>
+    /// Where should the user be redirected to after resolving the payload? (Optional)
+    /// </summary>
+    [JsonPropertyName("return_url")]
+    public XummPayloadReturnUrl? ReturnUrl { get; set; }
 }
