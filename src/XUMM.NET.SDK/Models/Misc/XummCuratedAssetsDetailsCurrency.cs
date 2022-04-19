fc419462707fd81c4f92a71c55cd886165d4238b
Dominique Blomsma <49249239+DominiqueBlomsma@users.noreply.github.com>
2022-04-19 08:31:25 +0200

    Added CurrencyFormatted to XummCuratedAssetsDetailsCurrency that formats currency codes as Standard Currency Codes. (#12)

--- src/XUMM.NET.SDK/Models/Misc/XummCuratedAssetsDetailsCurrency.cs
+++ src/XUMM.NET.SDK/Models/Misc/XummCuratedAssetsDetailsCurrency.cs
@@ -1,28 +1,33 @@
 ﻿using System.Text.Json.Serialization;
+using XUMM.NET.SDK.Extensions;
 
-namespace XUMM.NET.SDK.Models.Misc
+namespace XUMM.NET.SDK.Models.Misc;
+
+public class XummCuratedAssetsDetailsCurrency
 {
-    public class XummCuratedAssetsDetailsCurrency
-    {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
 
-        [JsonPropertyName("issuer_id")]
-        public int IssuerId { get; set; }
+    [JsonPropertyName("issuer_id")]
+    public int IssuerId { get; set; }
 
-        [JsonPropertyName("issuer")]
-        public string Issuer { get; set; } = default!;
+    [JsonPropertyName("issuer")]
+    public string Issuer { get; set; } = default!;
 
-        [JsonPropertyName("currency")]
-        public string Currency { get; set; } = default!;
+    [JsonPropertyName("currency")]
+    public string Currency { get; set; } = default!;
 
-        [JsonPropertyName("name")]
-        public string Name { get; set; } = default!;
+    /// <summary>
+    /// Formatted Currency Code supporting 3 chars, HEX and XLS15d HEX currency codes
+    /// </summary>
+    public string CurrencyFormatted => Currency.ToFormattedCurrency();
 
-        [JsonPropertyName("avatar")]
-        public string Avatar { get; set; } = default!;
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = default!;
 
-        [JsonPropertyName("shortlist")]
-        public int Shortlist { get; set; }
-    }
+    [JsonPropertyName("avatar")]
+    public string Avatar { get; set; } = default!;
+
+    [JsonPropertyName("shortlist")]
+    public int Shortlist { get; set; }
 }
