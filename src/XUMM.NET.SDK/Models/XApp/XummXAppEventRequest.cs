53e439015963f2d3e4e0b11470c1d851a362d66b
Dominique Blomsma <dominique_blomsma@hotmail.com>
2022-12-13 14:42:40 +0100

    Code cleanup and warning fixes

--- src/XUMM.NET.SDK/Models/XApp/XummXAppEventRequest.cs
+++ src/XUMM.NET.SDK/Models/XApp/XummXAppEventRequest.cs
@@ -2,7 +2,7 @@
 
 namespace XUMM.NET.SDK.Models.XApp;
 
-public  class XummXAppEventRequest : XummXAppPushRequest
+public class XummXAppEventRequest : XummXAppPushRequest
 {
     /// <summary>
     /// Only create the event in the user's Event list, don't send a push notification
