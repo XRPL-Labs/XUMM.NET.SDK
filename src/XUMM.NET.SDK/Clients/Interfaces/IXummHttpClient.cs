b833c6fd56e018cd71622ff1b5db00035ec6e740
Dominique Blomsma <dominique_blomsma@hotmail.com>
2023-06-07 17:18:35 +0200

    Moved URL part platform/ to endpoint parameters

--- src/XUMM.NET.SDK/Clients/Interfaces/IXummHttpClient.cs
+++ src/XUMM.NET.SDK/Clients/Interfaces/IXummHttpClient.cs
@@ -5,10 +5,13 @@ namespace XUMM.NET.SDK.Clients.Interfaces;
 
 public interface IXummHttpClient
 {
+    Task<T> GetAsync<T>(HttpClient client, string endpoint);
     Task<T> GetAsync<T>(string endpoint);
     Task<T> GetPublicAsync<T>(string endpoint);
     Task<T> PostAsync<T>(string endpoint, object content);
     Task<T> PostAsync<T>(string endpoint, string json);
+    Task<T> PostAsync<T>(HttpClient client, string endpoint, string json);
     Task<T> DeleteAsync<T>(string endpoint);
+    Task<T> DeleteAsync<T>(HttpClient client, string endpoint);
     HttpClient GetHttpClient(bool setCredentials);
 }
