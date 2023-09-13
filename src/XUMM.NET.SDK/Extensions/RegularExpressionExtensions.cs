8d7e9d64ad7b7de1e5f928f0293d10b5d1f00dc7
Dominique Blomsma <dominique_blomsma@hotmail.com>
2023-09-13 21:28:52 +0200

    Account and SHA512H regex

--- src/XUMM.NET.SDK/Extensions/RegularExpressionExtensions.cs
+++ src/XUMM.NET.SDK/Extensions/RegularExpressionExtensions.cs
@@ -4,8 +4,8 @@ namespace XUMM.NET.SDK.Extensions;
 
 public static class RegularExpressionExtensions
 {
-    private static readonly Regex RgxAccount = new("^r");
-    private static readonly Regex RgxHookHash = new("^[A-Fa-f0-9]{64}$");
+    private static readonly Regex RgxAccount = new(@"^r[1-9A-HJ-NP-Za-km-z]{25,33}$");
+    private static readonly Regex RgxSHA512H = new("^[A-Fa-f0-9]{64}$");
     private static readonly Regex RgxUuid = new(@"^[a-f0-9]{8}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{12}$");
 
     public static bool IsAccountAddress(this string? input)
@@ -13,9 +13,14 @@ public static class RegularExpressionExtensions
         return input != null && RgxAccount.IsMatch(input);
     }
 
-    public static bool IsValidHookHash(this string? input)
+    /// <summary>
+    /// Many objects in the XRP Ledger, particularly transactions and ledgers, are uniquely identified by a 256-bit hash value.
+    /// This value is typically calculated as a "SHA-512Half", which calculates a SHA-512  hash from some contents, then takes the first half of the output.
+    /// (That's 256 bits, which is 32 bytes, or 64 characters of the hexadecimal representation.)
+    /// </summary>
+    public static bool IsSHA512H(this string? input)
     {
-        return input != null && RgxHookHash.IsMatch(input);
+        return input != null && RgxSHA512H.IsMatch(input);
     }
 
     public static bool IsValidUuid(this string? input)
