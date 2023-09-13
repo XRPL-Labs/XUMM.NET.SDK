4010cd1fc5ea7064b079e2b1fec50f2c17ff159f
Dominique Blomsma <dominique_blomsma@hotmail.com>
2023-09-13 13:54:59 +0200

    Added HookHash models

---
+++ src/XUMM.NET.SDK/Models/Misc/HookHash/XummHookHashCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace XUMM.NET.SDK.Models.Misc;
+
+public class XummHookHashCreator
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("mail")]
+    public string? Mail { get; set; }
+
+    [JsonPropertyName("site")]
+    public string? Site { get; set; }
+}
