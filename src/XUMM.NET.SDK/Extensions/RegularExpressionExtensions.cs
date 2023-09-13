1a8d9059daa8b32710e790ea37676c46ee9cf7d7
Dominique Blomsma <dominique_blomsma@hotmail.com>
2023-09-13 13:58:33 +0200

    Added HookHash regex

--- src/XUMM.NET.SDK/Extensions/RegularExpressionExtensions.cs
+++ src/XUMM.NET.SDK/Extensions/RegularExpressionExtensions.cs
@@ -5,11 +5,17 @@ namespace XUMM.NET.SDK.Extensions;
 public static class RegularExpressionExtensions
 {
     private static readonly Regex RgxAccount = new("^r");
+    private static readonly Regex RgxHookHash = new("^[A-Fa-f0-9]{64}$");
     private static readonly Regex RgxUuid = new(@"^[a-f0-9]{8}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{12}$");
 
     public static bool IsAccountAddress(this string? input)
     {
         return input != null && RgxAccount.IsMatch(input);
+    }
+
+    public static bool IsValidHookHash(this string? input)
+    {
+        return input != null && RgxHookHash.IsMatch(input);
     }
 
     public static bool IsValidUuid(this string? input)
