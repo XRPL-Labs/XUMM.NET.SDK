1c25035954d285cb59ef2d8e27ddc764f23ce954
Dominique Blomsma <49249239+DominiqueBlomsma@users.noreply.github.com>
2023-09-14 14:13:20 +0200

    Get HookHash (#37)
    * Fixed check on actual deserialized `XummApiError` with filled Error
    
    * Added HookHash models
    
    * Added HookHash regex
    
    * Account and SHA512H regex
    
    * Renamed models
    
    * Added hook info requests to `IXummMiscClient`
    
    * Added Unit tests
    
    * Added HookHash blazor example

---
+++ src/XUMM.NET.SDK/Models/Misc/HookHash/XummHookInfoCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace XUMM.NET.SDK.Models.Misc;
+
+public class XummHookInfoCreator
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("mail")]
+    public string? Mail { get; set; }
+
+    [JsonPropertyName("site")]
+    public string? Site { get; set; }
+}
